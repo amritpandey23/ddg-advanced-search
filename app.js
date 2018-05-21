ab55cb58160a1047675d932c8f43a440f64cb0c7
a-pandey <spanit007@outlook.com>
2018-05-21 16:03:31 +0530

    added search object model to store searches, changed few comments, added safe and strict to the options

--- app.js
+++ app.js
@@ -13,6 +13,13 @@
  * hence I've created a method that produces an object containing
  * them. To add new DOM object, just add its ID to elementsID array.
  */
+
+if (!localStorage.getItem('historyData')) {
+    var historyData = [];
+} else {
+    var historyData = JSON.parse(localStorage.getItem('historyData'));
+}
+
 var elementID = [
     "search_bar",
     "search_button",
@@ -21,12 +28,16 @@ var elementID = [
     "intitle_bar",
     "filetype",
     "strict",
+    "safe",
     "exclude_site_bar",
     "toggle_exclude_site",
     "exclude_term_bar",
-    "toggle_exclude_term"
+    "toggle_exclude_term",
+    "recent_search_list"
 ];
-elements = collectElements(elementID);
+var elements = collectElements(elementID);
+
+populateHistory(historyData);
 
 // Exclude data are optional hence they needs to be enabled by users to use them.
 // These two lines, add listeners to their respective checkboxes.
@@ -47,6 +58,7 @@ elements["search_button"].addEventListener('click', function(e) {
     // WARNING: href value of search button may contain template URL of old searches.
     if(!elements["search_bar"].value) {
         e.preventDefault();
+        return;
     } else {
 
         // Collect data/preferences from inputs and store in object named parameters.
@@ -63,14 +75,31 @@ elements["search_button"].addEventListener('click', function(e) {
             "site": elements["site_bar"].value,
             "inurl": elements["inurl_bar"].value,
             "intitle": elements["intitle_bar"].value,
-            "filetype": elements["filetype"].value
+            "filetype": elements["filetype"].value,
+            "strict": elements["strict"].checked,
+            "safe": elements["safe"].checked
         }
+        
+        // Creating new search object based on `Search` model.
+        var searchItem = new Search(parameters);
 
-        // Generate and set template URL to search button's href value.
-        e.target.href = createURL(parameters);
+        console.log(searchItem);
+
+        // Update search history data.
+        historyData.unshift(searchItem);
+        localStorage.setItem('historyData', JSON.stringify(historyData));
+
+        // Clear query field for next search.
+        elements["recent_search_list"].textContent = '';
+
+        // Add entry to the search list.
+        populateHistory(historyData);
+
+        // Finally set href value of search button to the URL generated
+        // from the search object.
+        e.target.href = searchItem.createURL();
     }
 });
-
 
 /**
  * REFACTORING
@@ -102,44 +131,103 @@ function toggleTextInput(toggler, inputTarget) {
     })
 };
 
+// [Function] -- Generate an HTML li element containing previous search info and appends
+// to the search history UL list.
+function populateHistory(historyData = []) {
+    historyData.forEach(function(search) {
+        var li = document.createElement("li");
+        var a = document.createElement('a');
+        a.href = search.url;
+        a.textContent = search.query;
+        a.target = "_blank";
+        li.appendChild(a);
+        elements["recent_search_list"].appendChild(li); 
+    });
+}
+
 /**
- * CREATE TEMPLATE URL
- * Description: This function returns a template url for search.
- * It takes an object argument that containes property like query,
- * site, inurl, intitle, filetype and so on.
+ * Creating a search model to effectively store data entered by
+ * users. Also helper methods to do tasks like generating url,
+ * population of history etc.
  * 
- * I've used if conditional to check if users have indeed provided
- * those values. Please assist if there is any cleaner way to do it.
+ * Accepts: para[Obj.] with properties query, site, inurl,
+ * intitle, filetype[null], strictSearch[false], safeOn[false]. 
  */
-function createURL(para) {
+class Search {
 
-    // Check if `strict` search is checked
-    if(strict.checked) {
-        para["query"] = '"' + para["query"] + '"';
+    constructor(para) {
+        this.query = para["query"];
+        this.site = para["site"] || '';
+        this.inurl = para["inurl"] || '';
+        this.intitle = para["intitle"] || '';
+        this.filetype = para["filetype"] || 'Null';
+        this.strictSearch = para["strict"] || false;
+        this.safeOn = para["safe"] || false;
+        this.url = this.createURL()
     }
 
-    // URL template only with search query
-    var url = `https://duckduckgo.com/?q=${encodeURIComponent(para["query"])}`;
-
-    // Adding site preference if passed by user
-    if(para["site"]) {
-        url += `+site:${para["site"]}`;
+    /**
+     * Create an object containing the preference passed by
+     * the user, this will be used to populate search entry
+     * in search entry list.
+     */
+    produceSearchInfo() {
+        return {
+            "query": this.query,
+            "site": this.site,
+            "inurl": this.inurl,
+            "intitle": this.intitle,
+            "filetype": this.filetype,
+            "strictSearch": this.strictSearch,
+            "safeOn": this.safeOn
+        };
     }
 
-    // Adding inurl preference if passed by user
-    if(para["inurl"]) {
-        url += `+inurl:${para["inurl"]}`;
-    }
+    /**
+     * CREATE TEMPLATE URL
+     * Description: This function returns a template url for search.
+     * It takes an object argument that containes property like query,
+     * site, inurl, intitle, filetype and so on.
+     * 
+     * I've used if conditional to check if users have indeed provided
+     * those values. Please assist if there is any cleaner way to do it.
+     */
+    createURL() {
+        var para = this.produceSearchInfo();
 
-    // Adding intitle preference if passed by user
-    if(para["intitle"]) {
-        url += `+inurl:${para["intitle"]}`;
-    }
+        // Check if `strict` search is checked
+        if(para["strictSearch"]) {
+            para["query"] = '"' + para["query"] + '"';
+        }
 
-    // Adding filetype preference if passed by user
-    if (para["filetype"] != 'Null') {
-        url += `+filetype:${para["filetype"]}`;
-    }
+        // Enforce safe search searching if safe is on
+        if(para["safeOn"]) {
+            para["query"] = '!safeon ' + para["query"];
+        }
 
-    return url;
+        // URL template only with search query
+        var url = `https://duckduckgo.com/?q=${encodeURIComponent(para["query"])}`;
+
+        // Adding site preference if passed by user
+        if(para["site"]) {
+            url += `+site:${para["site"]}`;
+        }
+
+        // Adding inurl preference if passed by user
+        if(para["inurl"]) {
+            url += `+inurl:${para["inurl"]}`;
+        }
+
+        // Adding intitle preference if passed by user
+        if(para["intitle"]) {
+            url += `+inurl:${para["intitle"]}`;
+        }
+
+        // Adding filetype preference if passed by user
+        if (para["filetype"] != 'Null') {
+            url += `+filetype:${para["filetype"]}`;
+        }
+
+        return url;
+    }
 }
