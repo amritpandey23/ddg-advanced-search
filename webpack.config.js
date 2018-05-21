a039f4fc0737d1795faa88bc9f26625e6e2784c8
a-pandey <spanit007@outlook.com>
2018-05-21 16:49:06 +0530

    webpack configuration file

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+
+    entry: './src/app.js',
+    
+    output: {
+        path: path.resolve(__dirname),
+        filename: 'main.js'
+    },
+
+    module: {
+        rules: [
+            { test: /\.js$/, use: 'babel-loader' }
+        ]
+    },
+
+    mode: 'production'
+
+}
